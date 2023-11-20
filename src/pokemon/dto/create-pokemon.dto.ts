811da78b9734b4df97eed01b827563c961c76b5a
MarceloGodoy <magodoyb@gmail.com>
2023-11-20 15:48:33 -0300

    Fin sección 7

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,11 @@
+import { IsInt, IsPositive, MinLength, IsString, Min } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
